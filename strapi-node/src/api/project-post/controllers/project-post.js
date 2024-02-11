14bce946dd094a4df4fef363f42170a6fcef0856
EthanShoeDev <13422990+EthanShoeDev@users.noreply.github.com>
2024-02-11 16:44:06 -0500

    Remove strapi

--- strapi-node/src/api/project-post/controllers/project-post.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * project-post controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::project-post.project-post');
